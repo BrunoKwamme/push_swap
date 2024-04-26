47f70f521055a6a9be668d2be7213bc0272d21cf
bkwamme <ambsouza@gmail.com>
2024-04-25 23:35:04 -0300

    reverse file

---
+++ source/reverse_move.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_move.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkwamme <bkwamme@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 20:21:54 by bkwamme           #+#    #+#             */
+/*   Updated: 2024/04/25 20:26:06 by bkwamme          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	rra(t_stack **a)
+{
+	t_stack *last;
+	t_stack *penultimate;
+
+	last = NULL;
+	penultimate = NULL;
+	if ((*a) == NULL)
+		return ;
+	penultimate = (*a);
+	last = (*a);
+	while (last->next != NULL)
+		last = last->next;
+	while (penultimate->next != last)
+		penultimate = penultimate->next;
+	penultimate->next = NULL;
+	last->next = (*a);
+	*a = last;
+}
+
+void	rrb(t_stack **b)
+{
+	t_stack *last;
+	t_stack *penultimate;
+
+	last = NULL;
+	penultimate = NULL;
+	if ((*b) == NULL)
+		return ;
+	penultimate = (*b);
+	last = (*b);
+	while (last->next != NULL)
+		last = last->next;
+	while (penultimate->next != last)
+		penultimate = penultimate->next;
+	penultimate->next = NULL;
+	last->next = (*b);
+	*b = last;
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	if ((*a) == NULL || (*b) == NULL)
+		return ;
+	rra(a);
+	rrb(b);
+}
